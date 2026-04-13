e2586b3d1b04a545e9d48121c373e8b3e3449701
Daniastud <d.astudillod@uniandes.edu.co>
2026-04-13 16:03:51 +0000

    mejor fortnite

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const listaSuperior = [
   "assets/img/Fishstick.png",
   "assets/img/ikonik.png",
   "assets/img/Peely (1).png",
-  "assets/img/Marshmello.png"
+  "assets/img/Marshmello.png",
+  "assets/img/Skull Trooper.png",
+  "assets/img/TheGrefg.png",
+  "assets/img/Peely Bone.png"
 ];
 
 const listaInferior = [
   "assets/img/piernas carburo.png",
   "assets/img/piernas galaxy.png",
   "assets/img/piernas omega.png",
-  "assets/img/piernas teknique.png"
+  "assets/img/piernas teknique.png",
+  "assets/img/Piernas Peely (1).png",
+  "assets/img/Piernas Skull Trooper.png",
+  "assets/img/Piernas TheGrefg.png"
 ];
 
 const superior = document.getElementById("superior");
@@ -25,12 +31,12 @@ function generarBestia() {
   inferior.src = listaInferior[i];
 }
 
-boton.addEventListener("click", function() {
+boton.addEventListener("click", () => {
   generarBestia();
-  
+
   if (musica.paused) {
-    musica.volume = 0.6;
-    musica.play().catch(error => console.log("Esperando interacción..."));
+    musica.volume = 0.5;
+    musica.play();
   }
 });
 
